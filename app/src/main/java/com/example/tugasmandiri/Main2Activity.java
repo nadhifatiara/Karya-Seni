95cc4a8aab1f34cfa25649597ba4993e69e2a86b
nadhifa <nadhifatiaraputri@gmail.com>
2019-12-17 15:03:46 +0700

    login google, camera

--- app/src/main/java/com/example/tugasmandiri/Main2Activity.java
+++ app/src/main/java/com/example/tugasmandiri/Main2Activity.java
@@ -1,14 +1,104 @@
 package com.example.tugasmandiri;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Main2Activity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_PICTURE = 1002;
+    ImageButton imgCamera;
+    Button btnLogout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        imgCamera = findViewById(R.id.btnImage);
+        btnLogout = findViewById(R.id.logout);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        imgCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if (imageTakeIntent.resolveActivity(getPackageManager())  != null ){
+                    startActivityForResult(imageTakeIntent,REQUEST_IMAGE_PICTURE);
+                }
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+
+                GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(Main2Activity.this, gso);
+                mGoogleSignInClient.signOut();
+
+
+
+                new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest
+                        .Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse graphResponse) {
+
+                        LoginManager.getInstance().logOut();
+
+                    }
+                }).executeAsync();
+                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(Main2Activity.this,"Back button is disabled",Toast.LENGTH_SHORT).show();
+        return;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_PICTURE && resultCode==RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+
+            Intent intent = new Intent(Main2Activity.this, ResultActivity.class);
+            intent.putExtra(ResultActivity.EXTRA_IMAGE_RESULT, imageBitmap);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/tugasmandiri/MainActivity.java
+++ app/src/main/java/com/example/tugasmandiri/MainActivity.java
@@ -1,40 +1,43 @@
 package com.example.tugasmandiri;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.facebook.CallbackManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1023;
     Button btnSigIn, btnSignUp, btnGoogle, btnFb;
     EditText etEmail, etPass;
     TextView forgotPass;
 
+    CallbackManager callbackManager;
     private FirebaseAuth mAuth;
-    private static final String TAG = "AndroidClarified";
-    private GoogleSignInClient googleSignInClient;
-    private SignInButton googleSignInButton;
+
+    ProgressDialog progressDialog;
 
     public void onStart() {
         super.onStart();
@@ -54,8 +57,14 @@ public class MainActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.EdtEmail);
         etPass = findViewById(R.id.EdtPass);
         forgotPass = findViewById(R.id.tvForgotPass);
-
         btnGoogle = findViewById(R.id.google);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            finish();
+        }
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -68,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         btnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                loginGoogle();
             }
         });
 
@@ -106,12 +115,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
         forgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,5 +131,65 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i2);
             }
         });
+    }
+
+    private void loginGoogle(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    //google login
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        if(progressDialog == null){
+            progressDialog = new ProgressDialog(MainActivity.this);
+        }
+        progressDialog.setMessage("Authenticating");
+        progressDialog.show();
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authetintication Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        progressDialog.dismiss();
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+
+            }
+        }
     }
 }
